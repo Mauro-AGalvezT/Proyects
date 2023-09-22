9207b78ddf6eaa634f80684125cd5b4971ae3b67
MauroGalvezT <mauro.galvez@tdilatam.com>
2023-09-22 00:36:45 -0500

    cambios

--- sun_flowers/main.js
+++ sun_flowers/main.js
@@ -1,52 +1,91 @@
 import * as THREE from './node_modules/three';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 
-// Configuración básica de la escena
+const loader = new GLTFLoader();
+let model, model2, model3, model4;
+let initialY = 0;
+let targetY = 0.8;
+let ascending = true;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+scene.background = new THREE.Color(0xffffff);
+const ambientLight = new THREE.AmbientLight(0x404040);
+ambientLight.intensity = 60;
+scene.add(ambientLight);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const edges = new THREE.EdgesGeometry(geometry);
+const material = new THREE.LineBasicMaterial({ color: 0xffff00 });
+const cube = new THREE.LineSegments(edges, material);
+cube.position.set(0, 0, 0);
+const whitePlaneGeometry = new THREE.PlaneGeometry(10, 10);
+const whiteMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const whitePlane = new THREE.Mesh(whitePlaneGeometry, whiteMaterial);
+whitePlane.rotation.x = -Math.PI / 2;
+whitePlane.position.set(0, -2, 0);
+scene.add(whitePlane);
+loader.load('Sunflower_1.glb', (gltf) => {
 
-// Crear una representación simplificada de una flor
-const flowerGroup = new THREE.Group();
+  model = gltf.scene;
+  model.position.set(0, 1, 0);
+  scene.add(model);
 
-// Tallo de la flor (un cilindro verde)
-const stemGeometry = new THREE.CylinderGeometry(0.05, 0.05, 1, 32);
-const stemTexture = new THREE.TextureLoader().load('ruta_a_la_textura_del_tallo.jpg');
-const stemMaterial = new THREE.MeshBasicMaterial({ map: stemTexture });
-const stem = new THREE.Mesh(stemGeometry, stemMaterial);
-flowerGroup.add(stem);
+  model2 = model.clone();
+  model2.position.set(-1, initialY, 0);
+  scene.add(model2);
 
-// Pétalos de la flor (geométricamente más complejos)
-const petalGeometry = new THREE.PlaneGeometry(0.4, 0.4);
-const petalTexture = new THREE.TextureLoader().load('ruta_a_la_textura_de_los_petalos.jpg');
-const petalMaterial = new THREE.MeshBasicMaterial({ map: petalTexture, transparent: true, side: THREE.DoubleSide });
-for (let i = 0; i < 6; i++) {
-    const petal = new THREE.Mesh(petalGeometry, petalMaterial);
-    petal.rotation.x = Math.PI / 2;
-    petal.position.set(Math.cos((i / 6) * Math.PI * 2) * 0.3, 0.7, Math.sin((i / 6) * Math.PI * 2) * 0.3);
-    flowerGroup.add(petal);
+  model3 = model.clone();
+  model3.position.set(0, initialY, 1);
+  scene.add(model3);
+
+  model4 = model.clone();
+  model4.position.set(1, initialY, 0);
+  scene.add(model4);
+
+  animateModel();
+});
+camera.position.z = 5;
+camera.position.y = 5;
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.fov = 45;
+camera.updateProjectionMatrix();
+
+function animateModel() {
+  // Actualiza la rotación del modelo en cada cuadro
+  if (model) {
+
+    if (ascending) {
+      // El modelo está subiendo
+      model.position.y += 0.003;
+      model2.position.y += 0.003;
+      model3.position.y += 0.003;
+      model4.position.y += 0.003;
+      if (model.position.y >= targetY) {
+        ascending = false;
+      }
+    } else {
+      // El modelo está bajando
+      model.position.y -= 0.002;
+      model2.position.y -= 0.002;
+      model3.position.y -= 0.002;
+      model4.position.y -= 0.002;
+      if (model.position.y <= initialY) {
+        ascending = true;
+      }
+    }
+    model4.rotation.y = 1
+    model2.rotation.y = -1
+  }
+
+  // Realiza el renderizado de la escena
+  renderer.render(scene, camera);
+
+  // Solicita el próximo cuadro de animación
+  requestAnimationFrame(animateModel);
 }
 
-scene.add(flowerGroup);
-
-// Fondo blanco
-renderer.setClearColor(0xffffff);
-
-// Posición de la cámara
-camera.position.z = 2;
-
-// Función de animación
-const animate = () => {
-    requestAnimationFrame(animate);
-
-    // Rotación de la flor
-    flowerGroup.rotation.y += 0.005;
-
-    // Renderiza la escena
-    renderer.render(scene, camera);
-};
-
-// Llama a la función de animación
-animate();
+animateModel();
